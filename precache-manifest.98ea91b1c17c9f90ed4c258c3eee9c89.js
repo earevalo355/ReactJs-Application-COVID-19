78769f47b8dd0e71522a8007a82df76d1fbe4e82
earevalo355 <earevalo355@gmail.com>
2020-04-30 21:18:29 +0800

    Updates

---
+++ precache-manifest.98ea91b1c17c9f90ed4c258c3eee9c89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e1d1ad4c00d58796621ae383e48f1cb",
+    "url": "/ReactJs-Application-COVID-19/index.html"
+  },
+  {
+    "revision": "5ffa6617c0e49fcdbd34",
+    "url": "/ReactJs-Application-COVID-19/static/css/main.8c4b7546.chunk.css"
+  },
+  {
+    "revision": "ab8c980552fda3f8cf10",
+    "url": "/ReactJs-Application-COVID-19/static/js/2.6a33c43b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/ReactJs-Application-COVID-19/static/js/2.6a33c43b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ffa6617c0e49fcdbd34",
+    "url": "/ReactJs-Application-COVID-19/static/js/main.2ae9f3f2.chunk.js"
+  },
+  {
+    "revision": "edd5274a92b036276844",
+    "url": "/ReactJs-Application-COVID-19/static/js/runtime-main.ecda046c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/ReactJs-Application-COVID-19/static/media/Covid.d7265326.png"
+  }
+]);
